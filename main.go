7186e96bfa84ffa80a6023aec6ed9cf72dd77640
LordGift <jarupath.j@kbtg.tech>
2018-11-05 11:30:23 +0700

    branch(selection statement)

--- main.go
+++ main.go
@@ -5,5 +5,25 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, World")
+	//shorthand declaration
+	name := "John"
+	fmt.Println("Hello, ", name)
+
+	//inline declaration
+	if inlineDeclare := "john"; inlineDeclare != "I'm Groot" {
+		fmt.Println(inlineDeclare + " not equal to 'I'm Groot'")
+	}
+
+	//switch-case is auto break (use fallthrough to continue if need)
+	switch "A" {
+	case "A":
+		fmt.Println("This is A case")
+		fallthrough
+	case "B":
+		fmt.Println("This is B case")
+	case "C":
+		fmt.Println("This is C case")
+	default:
+		fmt.Println("default case")
+	}
 }
