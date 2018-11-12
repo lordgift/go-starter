cb43fada79649f50d430f6731a7ee3a0f625b8f2
LordGift <jarupath.j@kbtg.tech>
2018-11-12 09:53:10 +0700

    fizzbuzz workshop

---
+++ workshop/mod.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func FizzBuzz(n int) string {
+
+	if n%3 == 0 && n%5 == 0 {
+		return "FizzBuzz"
+	} else if n%3 == 0 {
+		return "Fizz"
+	} else if n%5 == 0 {
+		return "Buzz"
+	} else {
+		return strconv.FormatInt(int64(n), 10)
+	}
+}
+
+func main() {
+	for no := 1; no <= 15; no++ {
+		fmt.Printf("%s,", FizzBuzz(no))
+	}
+}
